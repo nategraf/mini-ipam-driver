e1eb64d671d6e7d5eadce50ed478bdfc8bdc8b94
Victor "Nate" Graf <nategraf1@gmail.com>
2018-02-10 22:34:00 -0600

    add build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+pushd driver
+export GOARCH=amd64
+export GOOS=linux
+go build -o mini-ipam-driver.Linux.x64
+
+export GOARCH=386
+go build -o mini-ipam-driver.Linux.x86
+
+export GOOS=windows
+go build -o mini-ipam-driver.Windows.x86.exe
+
+export GOARCH=amd64
+go build -o mini-ipam-driver.Windows.x64.exe
+
+export GOARCH=
+export GOOS=
+popd
