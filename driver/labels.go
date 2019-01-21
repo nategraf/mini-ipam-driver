f27f00e59c3b2e23f86dbc379ca01c9e5c76e3a0
Victor "Nate" Graf <nategraf1@gmail.com>
2019-01-21 17:02:04 -0600

    shorten the label prefix

--- driver/labels.go
+++ driver/labels.go
@@ -2,7 +2,7 @@ package driver
 
 const (
 	// Prefix starts any labels from this driver.
-	Prefix = "com.github.mini"
+	Prefix = "mini"
 
 	// BridgeName label for bridge driver
 	CidrMaskLength = Prefix + ".cidr_mask_length"
